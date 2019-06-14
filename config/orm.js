1609398fa21234cc79385d8168ac0d577407ed85
alexcahn <alexbcahn@gmail.com>
2019-06-14 15:47:42 -0500

    orm functions set up

--- config/orm.js
+++ config/orm.js
@@ -2,12 +2,19 @@ var connection = require("./connection");
 
 var orm = {
     selectAll: function(){
-
+        let queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, result) {
+          if (err) throw err;
+          console.log(result);
+        });
     },
     insertOne: function(){
+        let queryString = "INSERT INTO burgers SET (burger_name=?, devoured=?)";
+
 
     },
     updateOne: function(){
+        let queryString = "UPDATE burgers WHERE (burger_name=?, devoured=?)";
 
     }
 }
